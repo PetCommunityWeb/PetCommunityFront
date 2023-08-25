b57e8bb6b1bb2bc920752097af908513d744d46e
SongIsak <thddltkr10@naver.com>
2023-08-25 11:18:49 +0900

    feat: 병원등록 모달 띄우기

---
+++ src/store/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import Cookies from "js-cookie";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        role: null,
+        nickname: null,
+        email: null,
+        imageUrl: null
+    },
+    mutations: {
+        setUserRole(state, role) {
+            state.role = role;
+        },
+        setNickname(state, nickname) {
+            state.nickname = nickname;
+        },
+        setEmail(state, email) {
+            state.email = email;
+        },
+        setImageUrl(state, imageUrl) {
+            state.imageUrl = imageUrl;
+        },
+        logout(state) {
+            state.role = null;
+        }
+    },
+    actions: {
+        logout({ commit }) {
+            commit('logout');
+            localStorage.removeItem('accessToken');
+            console.log(localStorage.getItem('accessToken'))
+            Cookies.remove('refreshToken');
+            // 루트('/') 경로로 이동
+            this.$router.push('/');
+        }
+    }
+});
