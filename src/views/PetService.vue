dc0d6a9dac035d96435f9305d1b11b03fd4d745e
hyukjin <cdh5771@naver.com>
2023-08-28 18:23:04 +0900

    feat: 샵 정보 지도 아래 표시

--- src/views/PetService.vue
+++ src/views/PetService.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    <div id="kakao-map" style="width:2000px;height:800px;"></div>
+    <div id="kakao-map" style="width:1000px;height:400px;"></div>
+    <ul v-if="placesInfo.length" class="place-list">
+      <li v-for="place in placesInfo" :key="place.id" class="place-item">
+        <strong class="place-title">{{ place.place_name }}</strong>
+        <p class="place-details">
+          카테고리: <span class="place-category">{{ place.category_name }}</span><br>
+          연락처: <span class="place-phone">{{ place.phone ? place.phone : '없음' }}</span><br>
+          주소: <span class="place-address">{{ place.address_name }}</span><br>
+          거리: <span class="place-distance">{{ formatDistance(place.distance) }}</span><br>
+        </p>
+        <a :href="place.place_url" target="_blank" class="place-link">자세히 보기</a>
+        <hr class="place-separator">
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -12,7 +25,8 @@ export default {
       map: null,
       infoWindow: new kakao.maps.InfoWindow({ zIndex:1 }),
       isInfoWindowOpen: false,  // infoWindow 상태를 추적하기 위한 변수
-      currentMarker: null      // 현재 infoWindow가 연결된 마커를 추적
+      currentMarker: null,      // 현재 infoWindow가 연결된 마커를 추적
+      placesInfo: []
     };
   },
   mounted() {
@@ -50,9 +64,9 @@ export default {
     searchPlaces(location) {
       const places = new kakao.maps.services.Places();
 
-      // "애견샵" 키워드로 주변의 장소를 검색
       places.keywordSearch("애견샵", (result, status) => {
         if (status === kakao.maps.services.Status.OK) {
+          this.placesInfo = result; // 검색된 결과를 placesInfo에 저장
           for (let i = 0; i < result.length; i++) {
             this.displayMarker(result[i]);
           }
@@ -97,10 +111,66 @@ export default {
       this.isInfoWindowOpen = true;  // infoWindow가 열렸음을 상태에 반영합니다.
       this.currentMarker = marker;   // 현재 마커를 상태에 저장합니다.
     },
+
+    formatDistance(distance) {
+      if (!distance) return '알 수 없음';
+      const kmDistance = distance / 1000; // m를 km로 변환
+      return `${kmDistance.toFixed(2)} km`; // 소수점 둘째자리까지 반올림
+    },
   }
 }
 </script>
 
 <style scoped>
+
+#kakao-map {
+  width: 1000px;
+  height: 400px;
+  margin: 50px auto 0 auto;  /* 상단에 50px 여백을 추가하고 가운데 정렬 */
+}
+
+.place-list {
+  margin-top: 30px;
+  list-style-type: none; /* 글뭉단 기호 제거 */
+  padding-left: 0; /* 기본 패딩 제거 */
+}
+
+.place-item {
+  padding: 15px;
+  background-color: #f5f5f5;
+  border-radius: 5px;
+  margin-bottom: 20px;
+}
+
+.place-title {
+  font-size: 18px;
+  color: #333;
+  margin-bottom: 10px;
+  display: block;
+}
+
+.place-details {
+  font-size: 14px;
+}
+
+.place-category, .place-phone, .place-address, .place-distance {
+  color: #555;
+}
+
+.place-link {
+  display: inline-block;
+  margin-top: 10px;
+  padding: 5px 10px;
+  background-color: #007BFF;
+  color: #fff;
+  text-decoration: none;
+  border-radius: 3px;
+}
+
+.place-separator {
+  margin-top: 20px;
+  border: none;
+  border-top: 1px solid #ddd;
+}
 
 </style>
