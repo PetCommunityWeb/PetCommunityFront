3d5a00d5b734dcb7bab52b20c4f2c1e279a34e7e
Jongwoo Choi <muvnelik@naver.com>
2023-09-01 20:12:32 +0900

    feature: 페이지 CSS Style 작업

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,50 +1,64 @@
 <template>
-  <v-app-bar app color="blue accent-4">
-    <v-toolbar-title className="white--text">
-      <router-link to="/" class="toolbar-title-link">
-        Pet병
-      </router-link>
-    </v-toolbar-title>
+    <v-app-bar app color="beige">
+        <v-toolbar-title className="white--text">
+            <router-link to="/" class="toolbar-title-link">
+                <v-img src="dogline.png" class="mr-2"></v-img>
+            </router-link>
+        </v-toolbar-title>
 
-    <v-spacer></v-spacer>
-
-    <v-btn text to="/find-hospital" color="white" className="mx-2">병원찾기</v-btn>
-    <v-btn text to="/realtime-consult" color="white" className="mx-2">실시간 상담</v-btn>
-    <v-btn text to="/community" color="white" className="mx-2">커뮤니티</v-btn>
-    <v-btn text to="/pet-service" color="white" className="mx-2">애견 서비스</v-btn>
-    <v-btn text v-if="isLoggedIn && role === 'OWNER'" to="/management" color="white" className="mx-2">병원관리</v-btn>
-    <v-btn text v-if="!isLoggedIn" to="/login" color="white" class="mx-2">로그인</v-btn>
-    <v-btn text v-if="isLoggedIn" to="/mypage" color="white" className="mx-2">마이페이지</v-btn>
-    <v-btn text v-if="isLoggedIn" @click="logout" color="white" className="mx-2">로그아웃</v-btn> <!-- 로그아웃 버튼 추가 -->
-
-  </v-app-bar>
+        <v-spacer></v-spacer>
+        <div>
+            <v-btn text to="/find-hospital" class="custom-button">병원 찾기</v-btn>
+            <v-btn text to="/realtime-consult" class="custom-button">실시간 상담</v-btn>
+            <v-btn text to="/community" class="custom-button">커뮤니티</v-btn>
+            <v-btn text to="/pet-service" class="custom-button">애견 서비스</v-btn>
+            <v-btn text v-if="isLoggedIn && role === 'OWNER'" to="/management" class="custom-button">병원 관리
+            </v-btn>
+            <v-btn text v-if="!isLoggedIn" to="/login" class="custom-button">로그인</v-btn>
+            <v-btn text v-if="isLoggedIn" to="/mypage" class="custom-button">마이페이지</v-btn>
+            <v-btn text v-if="isLoggedIn" @click="logout" class="custom-button">로그아웃</v-btn><!-- 로그아웃 버튼 추가 -->
+        </div>
+    </v-app-bar>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex';
+import {mapActions, mapState} from 'vuex';
 
 export default {
-  computed: {
-    ...mapState({
-      isLoggedIn: state => !!state.role,
-      role: state => state.role
-    })
-  },
-  methods: {
-    ...mapActions(['logout']),
-  }
+
+    computed: {
+        ...mapState({
+            isLoggedIn: state => !!state.role,
+            role: state => state.role
+        })
+    },
+    methods: {
+        ...mapActions(['logout']),
+    }
 };
 </script>
 <style scoped>
 .toolbar-title-link {
-  text-decoration: none; /* 밑줄 제거 */
-  color: white; /* 텍스트 색상 */
-  font-weight: bold; /* 글씨 두께 */
-  font-size: 1.5rem; /* 글씨 크기 */
-  transition: color 0.3s ease; /* 색상 전환 효과 */
+    text-decoration: none; /* 밑줄 제거 */
+    color: white; /* 텍스트 색상 */
+    font-weight: bold; /* 글씨 두께 */
+    font-size: 1.5rem; /* 글씨 크기 */
+    transition: color 0.3s ease; /* 색상 전환 효과 */
 }
 
 .toolbar-title-link:hover {
-  color: rgba(255, 255, 255, 0.7); /* 마우스 오버 시 색상 */
+    color: rgba(255, 255, 255, 0.7); /* 마우스 오버 시 색상 */
+}
+
+.mr-2 {
+    width: 50px;
+    height: 50px;
+}
+
+.custom-button {
+    color: black;
+    font-stretch: condensed;
+    font-weight: bold; /* 글자 두께 설정 */
+    margin: 0 5px; /* 원하는 간격 설정 */
 }
 </style>
