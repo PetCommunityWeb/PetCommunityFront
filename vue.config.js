4c6db362559e7a7942b8ac485a3c5aa7d0a03d28
이삭 송 <51445608+Isaacsong-2@users.noreply.github.com>
2023-09-11 20:33:56 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,3 +3,13 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave:false
 })
+module.exports = {
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'https://pet-community-api.net',
+        changeOrigin: true
+      }
+    }
+    }
+}
