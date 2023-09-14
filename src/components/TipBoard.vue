dd9869eac144a3d39f540870df3939c97ba759d2
hyukjin <cdh5771@naver.com>
2023-09-14 20:23:49 +0900

    feat: 팁 검색어 추가 및 썸네일 업로드

--- src/components/TipBoard.vue
+++ src/components/TipBoard.vue
@@ -10,24 +10,54 @@
       <template v-slot:tip="{ tip }">
         {{ tip.user ? tip.user.username : 'Unknown' }}
       </template>
+
+      <template v-slot:item.title="{ item }">
+        <div class="d-flex align-center">
+          <v-img v-if="item.imageUrl" :src="item.imageUrl" class="thumbnail-image"></v-img>
+          {{ item.title }}
+        </div>
+      </template>
     </v-data-table>
-    <v-row class="mt-3">
-      <v-btn outlined color="blue" @click="writeClick"> 글쓰기 </v-btn>
+
+    <v-row class="mt-5 pa-3 elevation-1 rounded">
+      <v-col cols="2">
+        <v-combobox
+            v-model="searchType"
+            :items="['제목', '내용']"
+            dense
+        ></v-combobox>
+      </v-col>
+      <v-col cols="4">
+        <v-text-field
+            v-model="searchKeyword"
+            outlined
+            dense
+            prepend-inner-icon="mdi-magnify"
+            @input="searchTips"
+        ></v-text-field>
+      </v-col>
+<!--      <v-col cols="2">-->
+<!--        <v-btn class="large-btn search-btn" @click="searchTips" color="blue" outlined large left  tile>검색</v-btn>-->
+<!--      </v-col>-->
+      <v-col cols="4" class="text-right">
+        <v-btn class="large-btn" outlined color="blue" large @click="writeClick" tile> 글쓰기 </v-btn>
+      </v-col>
     </v-row>
   </v-container>
-
 </template>
 
 <script>
 import axios from "@/axios/axios-instance";
-// import AWS from 'aws-sdk';
 import {mapState} from "vuex";
 import {checkAuthAndAlert} from "@/methods/authChecker";
+import debounce from 'lodash/debounce';
 
 export default {
   name:'TipBoard',
   created() {
     this.fetchTips();
+    // debounce 적용
+    this.searchTips = debounce(this.searchTips, 100); // 300ms 디바운스
   },
   computed: {
     ...mapState('user', ['username']), // 사용자 이름 가져오기
@@ -41,10 +71,41 @@ export default {
         console.error("Error fetching tips:", error);
       }
     },
-    writeClick() {
-        if (!checkAuthAndAlert()) {
-            return;
+    async searchTips() {
+      if (!this.searchKeyword.trim()) {
+        this.fetchTips();
+        return;
+      }
+
+      if (this.searchType === '제목' && this.searchKeyword) {
+        try {
+          const response = await axios.get("/tips/title", {
+            params: {
+              keyword: this.searchKeyword
+            }
+          });
+          this.tips = response.data.map(item => ({ ...item, username: item.username }));
+          // eslint-disable-next-line no-empty
+        } catch (error) {
         }
+      }
+      else if (this.searchType === '내용' && this.searchKeyword) {
+        try {
+          const response = await axios.get("/tips/content", {
+            params: {
+              keyword: this.searchKeyword
+            }
+          });
+          this.tips = response.data.map(item => ({ ...item, username: item.username }));
+        } catch (error) {
+          console.error("Error searching by content:", error);
+        }
+      }
+    },
+    writeClick() {
+      if (!checkAuthAndAlert()) {
+        return;
+      }
       this.$router.push('/community/tip/create');
     },
     rowClick(tip) {
@@ -55,13 +116,30 @@ export default {
     return {
       requestBody: {},
       headers: [
-        { text: 'Title', value: 'title' },
-        { text: 'Username', value: 'username' },
+        { text: '제목', value: 'title' },
+        { text: '작성자', value: 'username' },
         { text: 'Like', value: 'likeCount'}
         // { text: 'Reg Date', value: 'regDt' }
       ],
       tips: [],
+      searchType: '제목', // 초기 검색 조건을 '제목'으로 설정
+      searchKeyword: ''    // 검색 키워드 저장
     };
   }
 };
 </script>
+
+<style scoped>
+.large-btn {
+  padding: 10px 16px !important;
+  border-radius: 25px !important;
+}
+.large-btn.search-btn {
+  margin-left: 0px !important;  /* 검색버튼을 왼쪽으로 30px 옮기는 부분 */
+}
+.thumbnail-image {
+  max-width: 50px;
+  max-height: 50px;
+  margin-right: 25px; /* 이 값을 조정하여 원하는 간격으로 설정 */
+}
+</style>
